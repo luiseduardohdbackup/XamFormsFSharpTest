095e1cd51199260051c1a5a10d1ce3d75d14fc72
Brad Pillow <bpillow@pillowsoft.com>
2014-05-30 19:54:06 -0400

    initial commit

---
+++ TestAndroid/MainActivity.fs
@@ -0,0 +1,51 @@
+﻿namespace TestAndroid
+
+open System
+open System.ComponentModel
+
+open Android.App
+open Android.Content
+open Android.OS
+open Android.Runtime
+open Android.Views
+open Android.Widget
+open Xamarin.Forms
+open Xamarin.Forms.Platform.Android
+
+type ButtonCodePage() as this =
+    inherit ContentPage()
+    [<Core.DefaultValue>] val mutable actt:Action<int>
+    let mutable count = 0
+    let button = new Button(Text = String.Format("Tap for click count!"))
+
+    do
+        button.Clicked.Add(fun _ ->
+                count <- count + 1
+                button.Text <- String.Format("{0} click{1}!", count, if count = 1 then "" else "s")
+            )
+
+        this.Content <- button
+
+type App() =
+    static member GetMainPage() =
+        new ButtonCodePage()
+
+[<Activity (Label = "TestAndroid", MainLauncher = true)>]
+type MainActivity () =
+    inherit AndroidActivity ()
+
+    let mutable count:int = 1
+
+    override this.OnCreate (bundle) =
+
+        base.OnCreate (bundle)
+
+        Xamarin.Forms.Forms.Init(this, bundle);
+        Xamarin.FormsMaps.Init(this, bundle);
+
+        this.SetPage(App.GetMainPage());
+
+
+
+
+
---
+++ TestiOS/AppDelegate.fs
@@ -0,0 +1,57 @@
+﻿namespace TestiOS
+
+open System
+open MonoTouch.UIKit
+open MonoTouch.Foundation
+
+open System
+open System.ComponentModel
+open Xamarin.Forms
+open Xamarin.Forms.Platform.iOS
+
+
+type ButtonCodePage() as this =
+    inherit ContentPage()
+    [<Core.DefaultValue>] val mutable actt:Action<int>
+    let mutable count = 0
+    let button = new Button(Text = String.Format("Tap for click count!"))
+
+    do
+        button.Clicked.Add(fun _ ->
+                count <- count + 1
+                button.Text <- String.Format("{0} click{1}!", count, if count = 1 then "" else "s")
+            )
+
+        this.Content <- button
+
+type App() =
+    static member GetMainPage() =
+        new ButtonCodePage()
+
+[<Register ("AppDelegate")>]
+type AppDelegate () =
+    inherit UIApplicationDelegate ()
+
+    [<Core.DefaultValue>] val mutable window:UIWindow
+
+    // This method is invoked when the application is ready to run.
+    override this.FinishedLaunching (app, options) =
+        Forms.Init()
+        this.window <- new UIWindow(UIScreen.MainScreen.Bounds)
+        this.window.RootViewController <- App.GetMainPage().CreateViewController()
+        this.window.MakeKeyAndVisible()
+        true
+
+type TestIt() =
+    interface IDisposable with
+        member x.Dispose(): unit = 
+            raise (System.NotImplementedException())
+
+
+
+module Main =
+    [<EntryPoint>]
+    let main args =
+        UIApplication.Main (args, null, "AppDelegate")
+        0
+
